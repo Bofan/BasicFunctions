9c763beaa415582313341b4a38cfcc3a1e7a5153
Bofan Chen <bofanchen98@gmail.com>
2020-12-24 21:18:14 -0800

    Update QuickSort.py
    Added header and docstrings.

--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -1,15 +1,29 @@
-# 
+#!/usr/bin/python3
 
-# This file uses the Lomuto partitioning scheme, 
-#   whereby the pivot for each subarray is the last element.
-# When an array has already been sorted, 
-#   this algorithm degrades to a runtime of O(n^2).
+"""
+This file uses the Lomuto partitioning scheme, 
+  whereby the pivot for each subarray is the last element.
+Best- and average-case runtimes for quicksort are O(nlogn).
+When an array has already been sorted, 
+  this algorithm degrades to a runtime of O(n^2).
+"""
 
-# The partitioning step.
 def partition(nums, lo, hi):
+    """
+    Sorts an array into two portions - 
+      one with elements smaller than a pre-selected "pivot" element
+      and one with elements larger.
+    Parameters
+        "nums" - The given sub/array.
+        "lo" - The index of the first element in the sub/array to be sorted.
+        "hi" - The index of the last element in the sub/array to be sorted.
+    Returns
+        "pivot" - The index of the pivot element, now in its correct position.
+    """
     pivot = hi
     i = lo
     while pivot > i:
+        # Move elements larger than the pivot element after said pivot.
         if nums[i] >= nums[pivot]:
             nums[i], nums[pivot-1], nums[pivot] = \
               nums[pivot-1], nums[pivot], nums[i]
@@ -18,8 +32,10 @@ def partition(nums, lo, hi):
             i += 1
     return pivot
 
-# 
 def quickSort(nums, lo, hi):
+    """
+    Recursively sorts the higher- and lower-valued halves of the sub/array.
+    """
     if lo < hi:
         p = partition(nums, lo, hi)
         quickSort(nums, lo, p-1)
