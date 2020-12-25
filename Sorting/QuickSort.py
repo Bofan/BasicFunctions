fd7ec34f38130f6f1920c6aa36c6acb7b1eb22fa
Bofan Chen <bofanchen98@gmail.com>
2020-12-24 20:58:09 -0800

    Corrected the algorithm.

--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -1,60 +1,34 @@
-# Quicksort is 
+# 
 
-# The Lomuto Partition selects the last element of all sub-arrays as the pivot.
-# This partition degrades to a runtime of O(n^2) when the array is already sorted.
-def lomutoPartition(nums, lo, hi):
-    pivot = nums[hi]
+# This file uses the Lomuto partitioning scheme, 
+#   whereby the pivot for each subarray is the last element.
+# When an array has already been sorted, 
+#   this algorithm degrades to a runtime of O(n^2).
+
+# The partitioning step.
+def partition(nums, lo, hi):
+    pivot = hi
     i = lo
-    from j in range(lo, hi):
-        if nums[j] > pivot:
-            # Swap the ith and jth numbers.
-            tmp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = tmp
+    while pivot > i:
+        if nums[i] >= nums[pivot]:
+            nums[i], nums[pivot-1], nums[pivot] = \
+              nums[pivot-1], nums[pivot], nums[i]
+            pivot -= 1
+        else:
             i += 1
-    # Swap the ith and last numbers.
-    tmp = nums[i]
-    nums[i] = nums[hi]
-    nums[hi] = nums[i]
-    # 
-    return i
-
-# The Hoare Partition 
-# This partition is more efficient that the Lomuto when...
-#   - 
-def hoarePartition(nums, lo, hi):
-    pivot = nums[(hi + lo) / 2]
-    i = lo - 1
-    j = hi + 1
-    # Forever loop.
-    while True:
-        while nums[i] < pivot:
-            i += 1
-        while nums[j] > pivot:
-            j -= 1
-        if i >= j:
-            return j
-        # Swap the ith and jth numbers.
-        tmp = nums[i]
-        nums[i] = nums[j]
-        nums[j] = tmp
+    return pivot
 
 # 
-def quickSort(nums, lo, hi, algo):
+def quickSort(nums, lo, hi):
     if lo < hi:
-        if algo == "lomuto":
-            p = lomutoPartition(nums, lo, hi)
-            quickSort(nums, lo, p-1, "lomuto")
-            quickSort(nums, p+1, hi, "lomuto")
-        elif algo == "hoare":
-            p = hoarePartition(nums, lo, hi)
-            quickSort(nums, lo, p, "hoare")
-            quickSort(nums, p+1, hi, "hoare")
+        p = partition(nums, lo, hi)
+        quickSort(nums, lo, p-1)
+        quickSort(nums, p+1, hi)
 
 def main():
     nums = []
-    print(quickSort(nums, 0, len(nums)-1), "lomuto")
-    print(quickSort(nums, 0, len(nums)-1), "hoare")
+    quickSort(nums, 0, len(nums)-1)
+    print(nums)
 
 if __name__ == "__main__":
     main()
